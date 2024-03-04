058a722525615ca1acf8a174204d7efd48b2fbfa
Werban <159199099+Werban@users.noreply.github.com>
2024-03-04 16:48:57 +0300

    Add files via upload

---
+++ GameState.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class GameState
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+        public GridValue[,] Grid { get; }
+        public Direction Dir { get; private set; }
+        public int Score { get; private set; }
+        public bool GameOver { get; private set; }
+
+
+        private readonly LinkedList<Direction> dirChanges = new LinkedList<Direction>();
+        private readonly LinkedList<Position> snakePositions = new LinkedList<Position>();
+        private readonly Random random = new Random();
+
+        public GameState(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            Grid = new GridValue[rows, cols];
+            Dir = Direction.Right;
+
+            AddSnake();
+            AddFood();
+
+        }
+
+        private void AddSnake()//добавление змеи в сетку
+        {
+            int r = Rows / 2;
+
+            for (int c = 0; c <= 3; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                snakePositions.AddFirst(new Position(r, c));
+            }
+        }
+
+        private IEnumerable<Position> EmptyPositions()//возвращает все пустые позиции
+        {
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Cols; c++)
+                {
+                    if (Grid[r, c] == GridValue.Empty)
+                    {
+                        yield return new Position(r, c);// возвращает элемент коллекции в итераторе и перемещает текущую позицию на следующий элемент.
+                    }
+                }
+            }
+        }
+
+        private void AddFood()//добавление еды
+        {
+            List<Position> empty = new List<Position>(EmptyPositions());
+
+            if (empty.Count == 0)//если нет пустых позиций возвращаем 
+            {
+                return;
+            }
+
+            Position pos = empty[random.Next(0, empty.Count)];
+            Grid[pos.Row, pos.Col] = GridValue.Food;
+        }
+        public Position HeadPosition()//возвращает позицию головы
+        {
+            return snakePositions.First.Value;
+        }
+        public Position TailPosition()//возвращает позицию хвоста
+        {
+            return snakePositions.Last.Value;
+        }
+        public IEnumerable<Position> SnakePositions()//возвращает позицию змеи
+        {
+            return snakePositions;
+        }
+
+        private void AddHead(Position pos)//Добавляет новую позицию к передней части змеи делая ее головой
+        {
+            snakePositions.AddFirst(pos);
+            Grid[pos.Row, pos.Col] = GridValue.Snake;//добавляем эту запись в массив
+        }
+
+        private void RemoveTail()
+        {
+            Position tail = snakePositions.Last.Value;//получаю позицию хвоста
+            Grid[tail.Row, tail.Col] = GridValue.Empty;//делаю позицию пустой в массиве
+            snakePositions.RemoveLast();
+        }
+
+        private Direction GetLastDirection()
+        {
+            if (dirChanges.Count == 0)
+            {
+                return Dir;
+            }
+
+            return dirChanges.Last.Value;
+        }
+
+        private bool CanChangeDirection(Direction newDir)
+        {
+            if(dirChanges.Count == 2)
+            {
+                return false;
+            }
+
+            Direction lastDir= GetLastDirection();
+            return newDir != lastDir && newDir != lastDir.Opposite();
+        }
+
+        public void ChangeDirection(Direction dir)//меняет направление змеи
+        {
+            //если может менять направление
+            if (CanChangeDirection(dir))
+            {
+                dirChanges.AddLast(dir);
+            }
+            
+        }
+        private bool OutsideGird(Position pos)//находится ли за сеткой
+        {
+            return pos.Row < 0 || pos.Row >= Rows || pos.Col < 0 || pos.Col >= Cols;
+        }
+
+        private GridValue WillHit(Position newHeadPos)//куда ударится
+        {
+            if (OutsideGird(newHeadPos))//если за пределами сетки
+            {
+                return GridValue.Outside;
+            }
+
+            if (newHeadPos == TailPosition())//совпадают ли позиции хвоста и головы
+            {
+                return GridValue.Empty;
+            }
+
+            return Grid[newHeadPos.Row, newHeadPos.Col];
+        }
+
+        public void Move()
+        {
+            if (dirChanges.Count > 0)
+            {
+                Dir = dirChanges.First.Value;
+                dirChanges.RemoveFirst();
+            }
+
+            Position newHeadPos = HeadPosition().Translate(Dir);//получ новое положение головы
+            GridValue hit = WillHit(newHeadPos);
+            if (hit == GridValue.Outside || hit == GridValue.Snake) 
+            {
+                GameOver = true;
+            }
+            else if (hit == GridValue.Empty) 
+            {
+                RemoveTail();
+                AddHead(newHeadPos);
+            }
+            else if(hit == GridValue.Food)
+            {
+                AddHead(newHeadPos);
+                Score++;
+                AddFood();
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Snake
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly Dictionary<GridValue, ImageSource> gridValToImage = new Dictionary<GridValue, ImageSource>
+        {
+            {GridValue.Empty, Images.Empty },
+            {GridValue.Snake, Images.Body },
+            {GridValue.Food, Images.Food },
+        };
+
+        private readonly Dictionary<Direction, int> dirToRotation = new Dictionary<Direction, int>()
+        {
+            {Direction.Left, 270},
+            {Direction.Right, 90},
+            {Direction.Up, 0},
+            {Direction.Down, 180},
+        };
+
+        private readonly int rows = 15, cols = 15;
+        private readonly Image[,] gridImages;
+        private GameState gameState;
+        private bool gameRunning;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            gridImages = SetupGrid();
+            gameState = new GameState(rows, cols);
+        }
+
+        private async Task RunGame()//запуск игры
+        {
+            Draw();
+            await ShowCountDown();
+            Overlay.Visibility = Visibility.Hidden;
+            await GameLoop();
+            await ShowGameOver();
+            gameState = new GameState(rows, cols);
+        }
+
+        private async void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (Overlay.Visibility == Visibility.Visible)
+            {
+                e.Handled = true;
+            }
+
+            if (!gameRunning)
+            {
+                gameRunning = true;
+                await RunGame();//ожидание запуска игры
+                gameRunning = false;
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (gameState.GameOver)
+            {
+                return;
+            }
+
+            switch (e.Key)
+            {
+                case Key.A:
+                    gameState.ChangeDirection(Direction.Left);
+                    break;
+                case Key.D:
+                    gameState.ChangeDirection(Direction.Right);
+                    break;
+                case Key.W:
+                    gameState.ChangeDirection(Direction.Up);
+                    break;
+                case Key.S:
+                    gameState.ChangeDirection(Direction.Down);
+                    break;
+            }
+        }
+
+        private async Task GameLoop()
+        {
+            while (!gameState.GameOver)
+            {
+                await Task.Delay(80);
+                gameState.Move();
+                Draw();
+            }
+        }
+
+        private Image[,] SetupGrid()//добавляет изовбражения в сетку игры
+        {
+            Image[,] images = new Image[rows, cols];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = cols;
+
+            for (int r = 0; r < rows; r++)//перебор всех позиций сетки
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    Image image = new Image
+                    {
+                        Source = Images.Empty,
+                        RenderTransformOrigin = new Point(0.5, 0.5)
+                    };
+
+                    images[r, c] = image;
+                    GameGrid.Children.Add(image);//добавляю как дочерний элемент игровой сетки
+                }
+            }
+            return images;
+        }
+
+        private void Draw()
+        {
+            DrawGrid();
+            DrawSnakeHead();
+            ScoreText.Text = $"SCORE {gameState.Score}";
+        }
+
+        private void DrawGrid()//просматривает состояние игры и обновл изображения сетки
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    GridValue gridVal = gameState.Grid[r, c];
+                    gridImages[r, c].Source = gridValToImage[gridVal];
+                    gridImages[r, c].RenderTransform = Transform.Identity;
+                }
+            }
+        }
+
+        private void DrawSnakeHead()
+        {
+            Position headPos = gameState.HeadPosition();//получ позицию головы
+            Image image = gridImages[headPos.Row, headPos.Col];//сеточное положение
+            image.Source = Images.Head;
+
+            int rotation = dirToRotation[gameState.Dir];//получ кол-во градусов
+            image.RenderTransform = new RotateTransform(rotation);//поворот головы
+        }
+
+        private async Task DrawDeadSnake()
+        {
+            List<Position> positions= new List<Position>(gameState.SnakePositions());//список содерж позиции змеи
+
+            for (int i = 0; i < positions.Count; i++)
+            {
+                Position pos = positions[i];
+                ImageSource source = (i == 0) ? Images.DeadHead : Images.DeadBody;//(состояние) ? [истинный путь]: [ложный путь];
+                gridImages[pos.Row, pos.Col].Source= source;
+                await Task.Delay(50);
+            }
+        }
+
+        private async Task ShowCountDown()//счетчик начала игры
+        {
+            for (int i = 3; i >= 1; i--)
+            {
+                OverlayText.Text = i.ToString();
+                await Task.Delay(500);
+            }
+        }
+
+        private async Task ShowGameOver()
+        {
+            await DrawDeadSnake();
+            await Task.Delay(1000);
+            Overlay.Visibility = Visibility.Visible;
+            OverlayText.Text = "PRESS ANY KEY TO START";
+        }
+    }
+}
